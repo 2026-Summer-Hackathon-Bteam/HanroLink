f3ec50dab4c7e6ad87729cf51f618ca6f95bea88
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2026-07-19 10:28:54 +0900

    ESlintとPrettierの設定。

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,20 +2,23 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'type script-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+        globals: globals.browser,
     },
   },
 ])
