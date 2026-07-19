4b2d63750d1952fdbdf35c1e43377c0930ed94b1
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2026-07-19 17:53:53 +0900

    packageの名前をcom.hanrolink;にして、ファイル構造を簡潔にする。

--- hanrolink-backend/src/main/java/com/hanrolink/HanrolinkBackendApplication.java
+++ hanrolink-backend/src/main/java/com/hanrolink/HanrolinkBackendApplication.java
@@ -1,4 +1,4 @@
-package com.hanrolink.hanrolink_backend;
+package com.hanrolink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hanrolink-backend/src/test/java/com/hanrolink/HanrolinkBackendApplicationTests.java
+++ hanrolink-backend/src/test/java/com/hanrolink/HanrolinkBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hanrolink.hanrolink_backend;
+package com.hanrolink;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
