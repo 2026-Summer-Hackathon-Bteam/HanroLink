1278f7734a246a30510414aaa31ce1f52ae6cb0b
mai051 <aco_miya@outlook.jp>
2026-08-01 12:44:55 +0900

    fix: コントローラーに不足していたAPIパス定義を追加

--- hanrolink-backend/src/main/java/com/hanrolink/business/controller/CurrentBusinessController.java
+++ hanrolink-backend/src/main/java/com/hanrolink/business/controller/CurrentBusinessController.java
@@ -2,14 +2,17 @@ package com.hanrolink.business.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hanrolink.business.api.BusinessApi;
 import com.hanrolink.business.response.CurrentBusinessGetResponse;
 
 @RestController
 public class CurrentBusinessController {
 
   // バイヤー、サプライヤー利用可能
+  @GetMapping(BusinessApi.V1.MINE)
   public ResponseEntity<CurrentBusinessGetResponse> get() {
 
     // TODO: 現在のユーザーに紐づく企業名を取得して、ResponseEntity.ok(response)で返す
