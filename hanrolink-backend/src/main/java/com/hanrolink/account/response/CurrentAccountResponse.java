f72ad24a7aa6069b4124a1c399a6414baf20b81e
mai051 <aco_miya@outlook.jp>
2026-08-01 14:29:51 +0900

    refactor: レスポンスDTOの定義を修正

--- hanrolink-backend/src/main/java/com/hanrolink/account/response/CurrentAccountResponse.java
+++ hanrolink-backend/src/main/java/com/hanrolink/account/response/CurrentAccountResponse.java
@@ -2,10 +2,7 @@ package com.hanrolink.account.response;
 
 import com.hanrolink.account.enums.BusinessUserAccountRegistrationApiStatus;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
 public record CurrentAccountResponse(
-  @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
   Role role,
 
   BusinessUserAccountRegistrationApiStatus businessUserAccountRegistrationStatus
