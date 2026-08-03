50520c7638f43b5f3ca3ce59d30c4d6c239b1a91
mai051 <aco_miya@outlook.jp>
2026-08-03 15:06:57 +0900

    fix: CurrentAccountResponseのnullableな項目を必須としてOpenAPIスキーマに明示

--- hanrolink-backend/src/main/java/com/hanrolink/account/response/CurrentAccountResponse.java
+++ hanrolink-backend/src/main/java/com/hanrolink/account/response/CurrentAccountResponse.java
@@ -2,9 +2,20 @@ package com.hanrolink.account.response;
 
 import com.hanrolink.account.enums.BusinessUserAccountRegistrationApiStatus;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CurrentAccountResponse(
+  @Schema(
+    requiredMode = Schema.RequiredMode.REQUIRED,
+    nullable = true
+  )
   CurrentAccountRole role,
 
+  
+  @Schema(
+    requiredMode = Schema.RequiredMode.REQUIRED,
+    nullable = true
+  )
   BusinessUserAccountRegistrationApiStatus businessUserAccountRegistrationStatus
 ) {
 
