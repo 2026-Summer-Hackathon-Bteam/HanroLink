5f59fbdef08b4b22c68115b8d2a749d7dbe5e4dd
mai051 <aco_miya@outlook.jp>
2026-08-16 10:35:11 +0900

    Revert "refactor: 処理内容が分かりやすいようコメントを修正"
    This reverts commit d7a167d6ee0ccffe799bea5911f3493b2d630ee7.

--- hanrolink-backend/src/main/java/com/hanrolink/product/service/SupplierProductManagementService.java
+++ hanrolink-backend/src/main/java/com/hanrolink/product/service/SupplierProductManagementService.java
@@ -270,7 +270,7 @@ public class SupplierProductManagementService {
       pendingFileUploadRepository.delete(mainImageUpload);
     }
 
-    // 指定された商品ストーリー画像の利用可否確認と画像の差し替え
+    // 指定された商品ストーリー画像の利用可否確認
     Map<Long, PendingFileUpload> storyImageUploadsByProductStoryId = new HashMap<>();
 
     for (ProductStoryUpdateRequest productStoryRequest : request.productStories()) {
