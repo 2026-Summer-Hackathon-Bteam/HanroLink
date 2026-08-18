4e6f0f2d2757dc5d72a2a4f5fa5ee0db296382d0
mai051 <aco_miya@outlook.jp>
2026-08-18 18:52:31 +0900

    refactor: チャット添付PDFのファイルサイズ上限を10MBに変更

--- hanrolink-backend/src/main/java/com/hanrolink/file/policy/PdfFilePolicy.java
+++ hanrolink-backend/src/main/java/com/hanrolink/file/policy/PdfFilePolicy.java
@@ -2,7 +2,7 @@ package com.hanrolink.file.policy;
 
 public final class PdfFilePolicy {
 
-  public static final long MAX_FILE_SIZE_BYTES = 5L * 1024 * 1024L;
+  public static final long MAX_FILE_SIZE_BYTES = 10L * 1024 * 1024L;
 
   private PdfFilePolicy() {}
 }
