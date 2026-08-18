01c146ea5d28848a87ac290a620302f27c54e640
mai051 <aco_miya@outlook.jp>
2026-08-18 18:09:21 +0900

    refactor: チャットの添付ファイルレスポンスに必須項目の修正に伴う変更

--- hanrolink-backend/src/main/java/com/hanrolink/chat/response/component/ChatMessageFileResponse.java
+++ hanrolink-backend/src/main/java/com/hanrolink/chat/response/component/ChatMessageFileResponse.java
@@ -5,25 +5,29 @@ import java.util.Objects;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ChatMessageFileResponse(
+  @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
   String displayFilename,
 
   @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
   String url,
 
   @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
-  String mimeType,
-
   Long fileSizeBytes
 ) {
   public ChatMessageFileResponse {
+    Objects.requireNonNull(
+      displayFilename,
+      "ChatMessageFileResponse.displayFilename must not be null"
+    );
+
     Objects.requireNonNull(
       url,
       "ChatMessageFileResponse.url must not be null"
     );
 
     Objects.requireNonNull(
-      mimeType,
-      "ChatMessageFileResponse.mimeType must not be null"
+      fileSizeBytes,
+      "ChatMessageFileResponse.fileSizeBytes must not be null"
     );
   }
 }
