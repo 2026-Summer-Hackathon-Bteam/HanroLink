ac95e559122a52fd5b2ae5ed739c3ba8681a9f0f
mai051 <aco_miya@outlook.jp>
2026-08-27 00:33:00 +0900

    refactor: 複数箇所などで使われている各入力項目の制約値をPolicyの定数へ変更

---
+++ hanrolink-backend/src/main/java/com/hanrolink/procurementrequest/policy/ProcurementRequestPolicy.java
@@ -0,0 +1,10 @@
+package com.hanrolink.procurementrequest.policy;
+
+public final class ProcurementRequestPolicy {
+
+  public static final int MAX_TITLE_LENGTH = 255;
+  public static final int MAX_DESCRIPTION_LENGTH = 1000;
+  public static final int MAX_REQUIRED_TRADE_TERMS_LENGTH = 1000;
+
+  private ProcurementRequestPolicy() {}
+}
---
+++ hanrolink-backend/src/main/java/com/hanrolink/product/policy/ProductPolicy.java
@@ -0,0 +1,13 @@
+package com.hanrolink.product.policy;
+
+public final class ProductPolicy {
+
+  public static final int MAX_NAME_LENGTH = 255;
+  public static final int MAX_CONTENT_QUANTITY_LENGTH = 255;
+  public static final int MAX_ALLERGY_INFORMATION_LENGTH = 255;
+  public static final int MAX_CERTIFICATION_INFORMATION_LENGTH = 1000;
+  public static final int MAX_CASE_SIZE_LENGTH = 255;
+  public static final int MAX_SALES_AREA_RESTRICTION_LENGTH = 255;
+
+  private ProductPolicy() {}
+}
